994b5512a718b28587d699c2b23af4d5be2fc66b
unknown <missingmarshall@gmail.com>
2020-05-03 20:27:12 +0100

    API requests now have optional parameters

--- src/apis/spacex.js
+++ src/apis/spacex.js
@@ -9,24 +9,50 @@ function SpaceXApi() {
         launches: 'https://api.spacexdata.com/v3/launches'
     }
     this.activeXHR = null;
+    this.dataCache = {};
 
 }
+
+SpaceXApi.prototype.getLaunches = function(year) {
+    var params = {};
+    if (year) {
+        params['launch_year'] = year;
+    }
+    this.requestData("launches", params)  
+};
 
 /**
  * Fire off a new request to the API
  *
  */
-SpaceXApi.prototype.requestData = function(endpoint) {
+SpaceXApi.prototype.requestData = function(endpoint, params) {
 
+    
     if (typeof this.endpoints[endpoint] === 'undefined') {
         console.warn('Invalid endpoint requested: '+endpoint);
         return;
     }
+    
+    params = params || {};
+    var queryParams = "?";
+    for (var param in params) {
+        queryParams+='&';
+        queryParams+= param+'='+params[param];
+    }
+
 
     // if there is already an xhr in progress, abort it before continuing
     if (this.activeXHR && this.activeXHR.readyState !== 4) {
         this.activeXHR.abort();
         delete this.activeXHR;
+    }
+
+    var requestUrl = this.endpoints[endpoint]+queryParams;
+
+    // use memoization to prevent making identical API requests more than once
+    if (this.dataCache[requestUrl]) {
+        SpaceXApi.prototype.processResponse.call(this, this.dataCache[requestUrl]);
+        return;
     }
 
     var xhr =  new XMLHttpRequest();
@@ -36,11 +62,11 @@ SpaceXApi.prototype.requestData = function(endpoint) {
         if (xhr.readyState !== 4) {
             return;
         }
-
+        this.dataCache[requestUrl] = xhr.responseText;
         SpaceXApi.prototype.processResponse.call(this, xhr.responseText);
     }.bind(this)
 
-    xhr.open('GET', this.endpoints[endpoint]);
+    xhr.open('GET', this.endpoints[endpoint]+queryParams);
     xhr.send();
 };
 
@@ -54,6 +80,12 @@ SpaceXApi.prototype.requestData = function(endpoint) {
 SpaceXApi.prototype.processResponse = function(res) {
     var data = JSON.parse(res);
     console.log(data);
+
+    if (typeof window.launchesLoaded === "function") {
+        launchesLoaded(data);
+    }
+
+
 };
 
 
